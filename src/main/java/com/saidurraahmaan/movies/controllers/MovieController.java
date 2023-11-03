f9db1f75eb9420b35d894f136d9b954e6b8802a8
saidurraahmaan <saidurraahmaan@gmail.com>
2023-11-04 01:46:30 +0600

    get movies by imdbid

--- src/main/java/com/saidurraahmaan/movies/controllers/MovieController.java
+++ src/main/java/com/saidurraahmaan/movies/controllers/MovieController.java
@@ -2,14 +2,17 @@ package com.saidurraahmaan.movies.controllers;
 
 import com.saidurraahmaan.movies.models.Movie;
 import com.saidurraahmaan.movies.services.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -20,5 +23,11 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> allMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public  ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId),HttpStatus.OK);
     }
 }
--- src/main/java/com/saidurraahmaan/movies/services/MovieService.java
+++ src/main/java/com/saidurraahmaan/movies/services/MovieService.java
@@ -3,10 +3,12 @@ package com.saidurraahmaan.movies.services;
 
 import com.saidurraahmaan.movies.models.Movie;
 import com.saidurraahmaan.movies.repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -16,5 +18,9 @@ public class MovieService {
     public List<Movie> allMovies(){
 
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
